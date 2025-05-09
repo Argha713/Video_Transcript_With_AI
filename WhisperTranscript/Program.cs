8ddd535030fc69a097bcddc98478f055823e61c8
Argha713 <sarkar.argha2@gmail.com>
2025-05-09 14:43:16 +0530

    test

--- WhisperTranscript/Program.cs
+++ WhisperTranscript/Program.cs
@@ -125,7 +125,7 @@ namespace WhisperTranscript
                 }
                 catch (JsonException ex)
                 {
-                    
+                    //Exception
                 }
             }
 
